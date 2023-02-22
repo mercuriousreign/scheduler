362f29b252fb39c1e5c1497ee3bb0613bcaf2347
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-22 01:47:21 -0500

    Implement class in DayListItem with stylization

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,13 @@
+import classNames from "classnames";
 import React from "react";
 
 export default function DayListItem(props) {
   const {name,spots,selected,setDay} = props;
+
+  let dayClass = classNames('day-list__item',{'day-list__item--selected':selected,'day-list__item--full':spots === 0})
+
   return (
-    <li onClick={()=>setDay(name)}>
+    <li onClick={()=>setDay(name)} className={dayClass}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
