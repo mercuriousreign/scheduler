0e70938718b6feaa70e5213fae2e7e4f9e2ae64a
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-22 19:35:22 -0500

    Finalize DayList and DayListItem component

---
+++ src/components/DayList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList (props){
+  const items = props.days.map((item) =>{
+    return(
+      <DayListItem
+        key = {item.id}
+        name = {item.name}
+        spots = {item.spots}
+        selected = {item.name === item.day}
+        setDay = {props.setDay}
+        />
+    );
+  });
+
+
+  return(<ul>
+    {items}
+  </ul>)
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,15 +1,26 @@
 import classNames from "classnames";
 import React from "react";
+import "styles/DayListItem.scss" 
 
 export default function DayListItem(props) {
   const {name,spots,selected,setDay} = props;
 
   let dayClass = classNames('day-list__item',{'day-list__item--selected':selected,'day-list__item--full':spots === 0})
 
+  const settingDay = () => {setDay(name)};
+
+  //()=>{setDay(name) 
   return (
     <li onClick={()=>setDay(name)} className={dayClass}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      {spots === 0 &&<h3 className="text--light">no spots remaining</h3>}
+      {spots === 1 && <h3 className="text--light">{formatSpots(spots)} spot remaining</h3>}
+      {spots >= 1 && <h3 className="text--light">{formatSpots(spots)} spots remaining</h3>}
+      
     </li>
   )
+}
+
+function formatSpots(x){
+  return parseFloat(x);
 }
