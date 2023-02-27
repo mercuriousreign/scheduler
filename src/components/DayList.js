c080ce5aaa21c025517954c1a3da063e7e31cc40
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-27 17:35:44 -0500

    Implement DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -9,7 +9,7 @@ export default function DayList (props){
         name = {item.name}
         spots = {item.spots}
         selected = {item.name === props.value}
-        setDay = {props.onChange}
+        setDay = {props.setDay}
         />
     );
   });
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -15,7 +15,7 @@ export default function DayListItem(props) {
       <h2 className="text--regular">{name}</h2>
       {spots === 0 &&<h3 className="text--light">no spots remaining</h3>}
       {spots === 1 && <h3 className="text--light">{formatSpots(spots)} spot remaining</h3>}
-      {spots >= 1 && <h3 className="text--light">{formatSpots(spots)} spots remaining</h3>}
+      {spots >= 2 && <h3 className="text--light">{formatSpots(spots)} spots remaining</h3>}
       
     </li>
   )
