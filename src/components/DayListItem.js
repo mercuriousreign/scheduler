13d348552dd81ee57f911f1617f8487322ff6b0a
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-09 19:19:19 -0500

    Refactor formatSpot function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,24 +1,26 @@
 import classNames from "classnames";
 import React from "react";
-import "styles/DayListItem.scss" 
+import "styles/DayListItem.scss"
 
 /**Rendering of individual day component on the side that allows user to click to see appointment on a individual day alongside how many spots remaining */
 export default function DayListItem(props) {
-  const {name,spots,selected,setDay} = props;
+  const { name, spots, selected, setDay } = props;
 
-  let dayClass = classNames('day-list__item',{'day-list__item--selected': selected ,'day-list__item--full':spots === 0})
- 
+  let dayClass = classNames('day-list__item', { 'day-list__item--selected': selected, 'day-list__item--full': spots === 0 })
+
+
+  function formatSpots(spots) {
+    if (spots === 1)
+      return '1 spot remaining';
+    else if (spots === 0)
+      return 'no spots remaining';
+    else return `${spots} spots remaining`;
+  }
+
   return (
-    <li onClick={()=>setDay(name)} className={dayClass} selected={selected} data-testid="day">
+    <li onClick={() => setDay(name)} className={dayClass} selected={selected} data-testid="day">
       <h2 className="text--regular">{name}</h2>
-      {spots === 0 &&<h3 className="text--light">no spots remaining</h3>}
-      {spots === 1 && <h3 className="text--light">{formatSpots(spots)} spot remaining</h3>}
-      {spots >= 2 && <h3 className="text--light">{formatSpots(spots)} spots remaining</h3>}
-      
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   )
-}
-
-function formatSpots(x){
-  return parseFloat(x);
 }
