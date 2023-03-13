8ae7a6929d9a023405530c8124aaf926c88efd23
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-13 00:43:31 -0400

    Fix header classname

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{time}</h4>
-      <hr className="appointment_separator" />
+      <hr className="appointment__separator"/>
     </header>
   )
 }
